bfb888377c4e336d1aab2fdef52d97c7871ce48d
szhong16 <81816710+szhong16@users.noreply.github.com>
2021-10-20 20:16:06 +0800

    Update operators and recipes

--- src/manualHTN.py
+++ src/manualHTN.py
@@ -18,125 +18,7 @@ def op_craft_wooden_axe_at_bench (state, ID):
 		return state
 	return False
 
-# new operators
-
-def op_craft_iron_axe_at_bench (state, ID):
-	if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.ingot[ID] >= 3 and state.stick[ID] >=2:
-		state.iron_axe[ID] += 1
-		state.ingot[ID] -= 3
-		state.stick[ID] -= 2
-		state.time[ID] -= 1
-		return state
-	return False
-
-def op_craft_stone_axe_at_bench (state, ID):
-	if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.cobble[ID] >= 3 and state.stick[ID] >=2:
-		state.stone_axe[ID] += 1
-		state.cobble[ID] -= 3
-		state.stick[ID] -= 2
-		state.time[ID] -= 1
-		return state
-	return False
-
-def op_craft_wooden_pickaxe_at_bench (state, ID):
-	if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.plank[ID] >= 3 and state.stick[ID] >=2:
-		state.wooden_pickaxe[ID] += 1
-		state.plank[ID] -= 3
-		state.stick[ID] -= 2
-		state.time[ID] -= 1
-		return state
-	return False
-
-def op_craft_iron_pickaxe_at_bench (state, ID):
-	if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.ingot[ID] >= 3 and state.stick[ID] >=2:
-		state.iron_pickaxe[ID] += 1
-		state.ingot[ID] -= 3
-		state.stick[ID] -= 2
-		state.time[ID] -= 1
-		return state
-	return False
-
-def op_craft_stone_pickaxe_at_bench (state, ID):
-	if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.cobble[ID] >= 3 and state.stick[ID] >=2:
-		state.stone_pickaxe[ID] += 1
-		state.cobble[ID] -= 3
-		state.stick[ID] -= 2
-		state.time[ID] -= 1
-		return state
-	return False
-
-def op_craft_furnace_at_bench (state, ID):
-	if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.cobble[ID] >= 8:
-		state.furnace[ID] += 1
-		state.cobble[ID] -= 8
-		state.time[ID] -= 1
-		return state
-	return False
-
-def op_wooden_pickaxe_for_coal (state, ID):
-	if state.time[ID] >= 4 and state.wooden_pickaxe[ID] >= 1:
-		state.coal[ID] += 1
-		state.wooden_pickaxe -= 1
-		state.time[ID] -= 4
-		return state
-	return False
-
-def op_iron_pickaxe_for_coal (state, ID):
-	if state.time[ID] >= 1 and state.iron_pickaxe[ID] >= 1:
-		state.coal[ID] += 1
-		state.iron_pickaxe -= 1
-		state.time[ID] -= 1
-		return state
-	return False
-
-def op_stone_pickaxe_for_coal (state, ID):
-	if state.time[ID] >= 2 and state.stone_pickaxe[ID] >= 1:
-		state.coal[ID] += 1
-		state.stone_pickaxe -= 1
-		state.time[ID] -= 2
-		return state
-	return False
-
-def op_iron_pickaxe_for_ore (state, ID):
-	if state.time[ID] >= 2 and state.iron_pickaxe[ID] >= 1:
-		state.ore[ID] += 1
-		state.iron_pickaxe -= 1
-		state.time[ID] -= 2
-		return state
-	return False
-
-def op_stone_pickaxe_for_ore (state, ID):
-	if state.time[ID] >= 4 and state.stone_pickaxe[ID] >= 1:
-		state.ore[ID] += 1
-		state.stone_pickaxe -= 1
-		state.time[ID] -= 4
-		return state
-	return False
-
-def op_iron_pickaxe_for_cobble (state, ID):
-	if state.time[ID] >= 1 and state.iron_pickaxe[ID] >= 1:
-		state.cobble[ID] += 1
-		state.iron_pickaxe -= 1
-		state.time[ID] -= 1
-		return state
-	return False
-
-def op_stone_pickaxe_for_cobble (state, ID):
-	if state.time[ID] >= 2 and state.stone_pickaxe[ID] >= 1:
-		state.cobble[ID] += 1
-		state.stone_pickaxe -= 1
-		state.time[ID] -= 2
-		return state
-	return False
-
-def op_wooden_pickaxe_for_cobble (state, ID):
-	if state.time[ID] >= 4 and state.wooden_pickaxe[ID] >= 1:
-		state.cobble[ID] += 1
-		state.wooden_pickaxe -= 1
-		state.time[ID] -= 4
-		return state
-	return False
-
+# your code here
 def op_craft_plank (state, ID):
 	if state.time[ID] >= 1 and state.wood[ID] >= 1:
 		state.plank[ID] += 4
@@ -161,56 +43,19 @@ def op_craft_bench (state, ID):
 		return state
 	return False
 
-def op_craft_rail_at_bench (state, ID):
-	if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.ingot[ID] >= 6 and state.stick[ID] >= 1:
-		state.rail[ID] += 16
-		state.ingot[ID] -= 6
-		state.stick[ID] -= 1
-		state.time[ID] -= 1
-		return state
-	return False
-
-def op_craft_cart_at_bench (state, ID):
-	if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.ingot[ID] >= 5:
-		state.cart[ID] += 1
-		state.ingot[ID] -= 5
-		state.time[ID] -= 1
-		return state
-	return False
-
-def op_smelf_ore_in_furnace (state, ID):
-	if state.time[ID] >= 5 and state.furnace[ID] >= 1 and state.coal[ID] >= 1 and state.ore[ID] >= 1:
-		state.ingot[ID] += 1
-		state.coal[ID] -= 1
-		state.ore[ID] -= 1
-		state.time[ID] -= 5
-		return state
-	return False
-
-def op_iron_axe_for_wood (state, ID):
-	if state.time[ID] >= 1 and state.iron_axe[ID] >= 1:
-		state.wood[ID] += 1
-		state.time[ID] -= 1
-		return state
-	return False
-
 def op_wooden_axe_for_wood (state, ID):
 	if state.time[ID] >= 2 and state.wooden_axe[ID] >= 1:
 		state.wood[ID] += 1
 		state.time[ID] -= 2
 		return state
 	return False
-
-def op_stone_axe_for_wood (state, ID):
-	if state.time[ID] >= 1 and state.stone_axe[ID] >= 1:
-		state.wood[ID] += 1
-		state.time[ID] -= 1
-		return state
-	return False
-
-# your code here
-
-pyhop.declare_operators (op_punch_for_wood, op_craft_wooden_axe_at_bench)
+# each of the operators are functions, this is a list of functions
+pyhop.declare_operators (op_punch_for_wood,
+						 op_craft_wooden_axe_at_bench,
+						 op_craft_plank,
+						 op_craft_stick,
+						 op_craft_bench,
+						 op_wooden_axe_for_wood)
 
 '''end operators'''
 
@@ -222,9 +67,19 @@ def produce_enough (state, ID, item, num):
 	return [('produce', ID, item), ('have_enough', ID, item, num)]
 
 def produce (state, ID, item):
-	if item == 'wood': 
+	if item == 'wood':
 		return [('produce_wood', ID)]
 	# your code here
+	elif item == 'stick':
+		return [('produce_stick', ID)]
+	elif item == 'plank':
+		return [('produce_plank', ID)]
+	elif item == 'bench':
+		if state.made_bench[ID] is True:
+			return False
+		else:
+			state.made_bench[ID] = True
+		return [('produce_bench', ID)]
 	elif item == 'wooden_axe':
 		# this check to make sure we're not making multiple axes
 		if state.made_wooden_axe[ID] is True:
@@ -246,94 +101,42 @@ def punch_for_wood (state, ID):
 def craft_wooden_axe_at_bench (state, ID):
 	return [('have_enough', ID, 'bench', 1), ('have_enough', ID, 'stick', 2), ('have_enough', ID, 'plank', 3), ('op_craft_wooden_axe_at_bench', ID)]
 
-# new recipe
-def craft_iron_axe_at_bench (state, ID):
-	return [('have_enough', ID, 'bench', 1), ('have_enough', ID, 'stick', 2), ('have_enough', ID, 'ingot', 3), ('op_craft_iron_axe_at_bench', ID)]
-
-def craft_stone_axe_at_bench (state, ID):
-	return [('have_enough', ID, 'bench', 1), ('have_enough', ID, 'stick', 2), ('have_enough', ID, 'cobble', 3), ('op_craft_iron_axe_at_bench', ID)]
-
-def craft_wooden_pickaxe_at_bench (state, ID):
-	return [('have_enough', ID, 'bench', 1), ('have_enough', ID, 'stick', 2), ('have_enough', ID, 'plank', 3), ('op_craft_wooden_pickaxe_at_bench', ID)]
-
-def craft_stone_pickaxe_at_bench (state, ID): #稿子 use for mining ore
-	return [('have_enough', ID, 'bench', 1), ('have_enough', ID, 'stick', 2), ('have_enough', ID, 'cobble', 3), ('op_craft_stone_pickaxe_at_bench', ID)]
-
-def craft_iron_pickaxe_at_bench (state, ID):
-	return [('have_enough', ID, 'bench', 1), ('have_enough', ID, 'stick', 2), ('have_enough', ID, 'ingot', 3), ('op_craft_iron_pickaxe_at_bench', ID)]
-
-def craft_furnace_at_bench (state, ID): #熔矿炉 somewhere you use to melt metal
-	return [('have_enough', ID, 'bench', 1), ('have_enough', ID, 'cobble', 8), ('op_craft_furnace_at_bench', ID)]
-
-def wooden_pickaxe_for_coal (state, ID):
-	return  [('op_wooden_pickaxe_for_coal'), ID]
-
-def iron_pickaxe_for_coal (state, ID):
-	return [('op_iron_pickaxe_for_coal'), ID]
-
-def stone_pickaxe_for_coal (state, ID):
-	return [('op_stone_pickaxe_for_coal'), ID]
-
-def iron_pickaxe_for_ore (state, ID):
-	return  [('op_iron_pickaxe_for_ore'), ID]
-
-def stone_pickaxe_for_ore (state, ID):
-	return  [('op_stone_pickaxe_for_ore'), ID]
-
-def iron_pickaxe_for_cobble (state, ID):
-	return [('op_iron_pickaxe_for_cobble'), ID]
-
-def stone_pickaxe_for_cobble (state, ID):
-	return [('op_stone_pickaxe_for_cobble'), ID]
-
-def wooden_pickaxe_for_cobble (state, ID):
-	return [('op_wooden_pickaxe_for_cobble'), ID]
-
+# your code here
 def craft_plank (state, ID):
-	return [('op_craft_plank'), ID]
+	return [('have_enough', ID, 'wood', 1), ('op_craft_plank', ID)]
 
 def craft_stick (state, ID):
-	return [('op_craft_stick'), ID]
+	return [('have_enough', ID, 'plank', 2), ('op_craft_stick', ID)]
 
 def craft_bench (state, ID):
-	return [('op_craft_bench'), ID]
-
-def craft_rail_at_bench (state, ID): #铁轨，扶手
-	return [('have_enough', ID, 'bench', 1), ('have_enough', ID, 'stick', 1), ('have_enough', ID, 'ingot', 6), ('op_craft_rail_at_bench', ID)]
-
-def craft_cart_at_bench (state, ID):
-	return [('have_enough', ID, 'bench', 1), ('have_enough', ID, 'ingot', 5), ('op_craft_cart_at_bench', ID)]
-
-def smelf_ore_in_furnace (state, ID):
-	return [('have_enough', ID, 'furnace', 1), ('have_enough', ID, 'coal', 1), ('have_enough', ID, 'ore', 1), ('op_smelf_ore_in_furnace', ID)]
-
-def iron_axe_for_wood (state, ID):
-	return [('have_enough', ID, 'iron_axe', 1), ('op_iron_axe_for_wood', ID)]
+	return [('have_enough', ID, 'plank', 4), ('op_craft_bench', ID)]
 
 def wooden_axe_for_wood (state, ID):
 	return [('have_enough', ID, 'wooden_axe', 1), ('op_wooden_axe_for_wood', ID)]
 
-def stone_axe_for_wood (state, ID):
-	return [('have_enough', ID, 'stone_axe', 1), ('op_stone_axe_for_wood', ID)]
-
-# your code here
-
-pyhop.declare_methods ('produce_wood', punch_for_wood)
+pyhop.declare_methods ('produce_wood', wooden_axe_for_wood, punch_for_wood)
+pyhop.declare_methods ('produce_plank', craft_plank)
+pyhop.declare_methods ('produce_stick', craft_stick)
+pyhop.declare_methods ('produce_bench', craft_bench)
 pyhop.declare_methods ('produce_wooden_axe', craft_wooden_axe_at_bench)
 
 '''end recipe methods'''
 
 # declare state
 state = pyhop.State('state')
+state.plank = {'agent': 0}
+state.stick = {'agent': 0}
 state.wood = {'agent': 0}
-state.time = {'agent': 4}
+state.time = {'agent': 46}
 # state.time = {'agent': 46}
 state.wooden_axe = {'agent': 0}
 state.made_wooden_axe = {'agent': False}
-# your code here 
+# your code here
+state.bench = {'agent': 0}
+state.made_bench = {'agent': False}
 
 # pyhop.print_operators()
 # pyhop.print_methods()
 
-pyhop.pyhop(state, [('have_enough', 'agent', 'wood', 1)], verbose=3)
-# pyhop.pyhop(state, [('have_enough', 'agent', 'wood', 12)], verbose=3)
+# pyhop.pyhop(state, [('have_enough', 'agent', 'wood', 1)], verbose=3)
+pyhop.pyhop(state, [('have_enough', 'agent', 'wood', 12)], verbose=3)
